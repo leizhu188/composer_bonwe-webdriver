91b48eae40cd48d3c84fd2dc16b9301a74e86d53
weibangwei <122783533@qq.com>
2019-04-02 16:18:35 +0800

    demo

---
+++ demo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bangweiwei
+ * Date: 2019-04-01
+ * Time: 11:37
+ */
+include_once __DIR__.'/vendor/autoload.php';
+var_dump(\Bonwe\WebDriver\WebDriverBy::id('demo'));
